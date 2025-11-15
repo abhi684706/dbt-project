5b92683edaa608d214920bdfaa02c4204141ea17
Abhi Ka <abhi684706@gmail.com>
2025-11-15 22:24:23 +0000

    test-Abhishek

--- models/raw_market_adverity/shopping_trends.sql
+++ models/raw_market_adverity/shopping_trends.sql
@@ -9,10 +9,5 @@
 }}
 
 select 
-Customer ID as CUSTOMER_ID,
-Age as AGE,
-Gender as GENDER,	
-Category as CATEGORY,
-Payment Method as PAYMENT_METHOD
-from
-{{ source('dbt_big', 'shopping_trends') }}
+*
+from {{ source('dbt_big', 'shopping_trends') }}
